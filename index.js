ee4a6349449ab47a254f7c2db7f5db2c87ac428e
Andrew Josey <JoganLosey@gmail.com>
2019-05-03 13:08:37 -0400

    back end chat settup

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
+
+
+// This is my "database"
+const db = [
+    'my Morytania locked Ultimate Ironman.'
+];
+
+
+// When GET request comes in,
+// send back all the messages.
+
+app.get('/api', (req, res) => {
+    res.json(db);
+});
+
+// when POST request comes in,
+// add message to array of messages.
+app.post('/api', (req, res) => {
+    console.log(req.body.message);
+    db.push(req.body.message);
+    res.json({
+        'message': req.body.message
+    })
+})
+
+
 
 app.listen(31337, () => {
     console.log("You are good to go shit head")
